e23fdd32942eaebff1ed296b1e83ea04b759f12f
ziniminisoldier <philipp.karst@gmx.de>
2023-05-31 11:37:16 +0200

    Combining Interfaces

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,11 @@
 package com.cc.java;
 
+import com.cc.java.bees.Drone;
+import com.cc.java.bees.HoneyBee;
+import com.cc.java.bees.Queen;
+import com.cc.java.bees.Worker;
+import com.cc.java.birds.Bird;
+
 public class App {
     
     public static void main(String[] args) {
@@ -14,10 +20,6 @@ public class App {
         pollObj(drone);
         pollObj(bird);
 
-        // HoneyBee ist abstrakt --> Fehler!
-        // HoneyBee bee = new HoneyBee();
-        // output(bee.doYourJob());
-
 
     }
 
@@ -27,6 +29,11 @@ public class App {
         output(bee.fly());
     }
 
+    // Überladen für Bird
+    private static void pollObj(Bird obj) {
+        output(obj.hasFeathers());
+        output(obj.fly());
+    }
    
     private static void output(String outputStr) {
         System.out.println(outputStr);
